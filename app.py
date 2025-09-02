f1327b33550feebbfd96affd12674929280152b8
muffunfase <non921566@gmail.com>
2025-09-02 01:01:06 -0400

    navbar

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from os import environ, getenv, scandir
 from flask import render_template
 from db import create_app, db
 
@@ -9,4 +8,12 @@ app.secret_key = 'fefefe'
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', page='home')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', page='about')
+
+@app.route('/uh')
+def other_page():
+    return render_template('other.html', page='other')
